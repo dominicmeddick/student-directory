ba3f203264e3ebec067aea47cd1a896090117576
dominicmeddick <dominicmeddick@gmail.com>
2021-03-24 08:32:36 +0000

    Add a list of students

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,15 +1,31 @@
 student_count = 11
 
+students = [
+  "Dr. Hannibal Lecter",
+  "Dark Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wickde Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+
 puts "The students of Villains Academy"
-puts "----------------"
-puts "Dir. Hannibal Lecter"
-puts"Dark Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "The Wickde Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts " ----------------- "
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+
 puts "Overall, we have #{student_count} great students"
