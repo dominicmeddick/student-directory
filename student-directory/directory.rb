854a762463176e1e6b5264ebaa15f581200a1d4a
dominicmeddick <dominicmeddick@gmail.com>
2021-03-24 08:44:46 +0000

    Add a list of students

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,5 +1,3 @@
-student_count = 11
-
 students = [
   "Dr. Hannibal Lecter",
   "Dark Vader",
@@ -13,19 +11,19 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
+def print_header
+  puts "The students of Villains Academy"
+  puts " ----------------- "
+end
+def print(names)
+  names.each do |name|
+  puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
 
-puts "The students of Villains Academy"
-puts " ----------------- "
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-
-puts "Overall, we have #{student_count} great students"
+print_header
+print(students)
+print_footer(students)
