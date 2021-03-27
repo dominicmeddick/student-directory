3414e8e949feaf5467468dd01dc93987c2b29713
dominicmeddick <dominicmeddick@gmail.com>
2021-03-27 15:01:10 +0000

    Step 10

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,51 +1,61 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :novemeber}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :novemeber}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer(students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    # input the students
+  when "2"
+    # show the students
+  when "9"
+    # this will case the program to terminate
+  else 
+    puts "I don't know what you mean, try again"
+  end
 end
 
 def print_header
   puts "The students of Villains Academy"
   puts " ----------------- "
 end
-def print(names)
-  names.each do |name|
-  puts "#{name[:name]} (#{name[:cohort]} cohort)"
+
+def print_students_list
+  @students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def interactive_menu
-  students = []
-  loop do
-   # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-  puts "9. Exit"
-
-    # 2. read the input and save it into a variable
-
-    selection = gets.chomp
-    # 3. do what the user has asked
-
-   case selection
-   when "1"
-     # input the students
-   when "2"
-    # show the students
-    when "9"
-      # this will case the program to terminate
-    else 
-      puts "I don't know what you mean, try again"
-    end
-  end
-end
+interactive_menu
