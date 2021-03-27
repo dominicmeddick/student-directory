a09ad6ed711c82345ca51491d388ef0443ceba99
dominicmeddick <dominicmeddick@gmail.com>
2021-03-27 16:12:33 +0000

    Step12

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -22,6 +22,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -30,7 +31,7 @@ def show_students
   print_students_list
   print_footer
 end
-
+ 
 def process(selection)
   case selection
   when "1"
@@ -39,6 +40,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else 
@@ -70,5 +73,15 @@ def save_students
   end
   file.close
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 
 interactive_menu
